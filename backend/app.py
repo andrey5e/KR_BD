7df75eb483ba54d8b60d1fab4d24848172a61644
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-11-21 14:25:10 +0300

    creating tables 1

--- backend/app.py
+++ backend/app.py
@@ -12,3 +12,36 @@ DATABASE_URL = "postgresql://postgres:/rental_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Марка
+class Brand(Base):
+    __tablename__ = "brands"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+# Модель
+class Model(Base):
+    __tablename__ = "models"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+# Паркинг
+class Parking(Base):
+    __tablename__ = "parkings"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+# Сотрудник
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+
+# Платёж
+class Payment(Base):
+    __tablename__ = "payments"
+    id = Column(Integer, primary_key=True, index=True)
+    contract_id = Column(Integer, ForeignKey("contracts.id"))
+    date = Column(Date)
+    amount = Column(Float)
+    contract = relationship("Contract", back_populates="payments")
